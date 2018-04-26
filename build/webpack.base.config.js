7898e86522b351c6223a6ffabc3cf718ef14bad4
Administrator <jianxi_lin@163.com>
2018-04-26 15:56:49 +0800

    添加支持less

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -20,6 +20,16 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 include: [resolve('src')]       
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
             }
         ]
     },
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,2 @@
+require('./menu.less')
 console.log('menu')
