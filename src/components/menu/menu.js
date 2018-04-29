f329f9071c1c94cdfdf316315b705dac793c7cb2
antbrothers <jianxi_lin@163.com>
2018-04-29 23:54:58 +0800

    增加一个菜单 模块

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,2 +1,11 @@
 require('./menu.less')
-console.log('menu')
+$(function () {
+    $('.nav-li').hover(function() {                     
+        $(this).find('img').attr('src',  $(this).find('img').attr('src').split('.png')[0]+'-w.png')
+        $('.category-nav-detail-wrapper').css('display','block');
+        $('.detail-title').text($(this).find('.nav-text').text())
+    }, function() { 
+        $(this).find('img').attr('src',  $(this).find('img').attr('src').split('-w')[0]+'.png')
+        $('.category-nav-detail-wrapper').css('display','none');
+    })
+})
