b663efd14872862acc3c6b00c7ce125fa90fcc9e
antbrothers <jianxi_lin@163.com>
2018-05-06 19:00:47 +0800

    修改菜单显示

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,14 +4,21 @@ $(function () {
     $('.nav-li').hover(function() {
         clearTimeout(time)   
         var _this = this 
-        $(_this).find('img').attr('src',  $(_this).find('img').attr('src').split('.png')[0]+'-w.png')
+        $(_this).find('.orgin-u').css('display', 'none')
+        $(_this).find('.orgin-w').css('display', 'block')
         $('.category-nav-detail-wrapper').css('display','block');
         time = setTimeout(function() {           
             $('.detail-title').text($(_this).find('.nav-text').text())
         }, 100)   
        
     }, function() { 
-        $(this).find('img').attr('src',  $(this).find('img').attr('src').split('-w')[0]+'.png')
-        $('.category-nav-detail-wrapper').css('display','none');       
+        $(this).find('.orgin-u').css('display', 'block')
+        $(this).find('.orgin-w').css('display', 'none') 
+        $('.category-nav-detail-wrapper').css('display','none');        
+    })
+    $('.category-nav-detail-wrapper').hover(function() {    
+        $('.category-nav-detail-wrapper').css('display','block');            
+    }, function() {
+        $(this).css('display','none');       
     })
 })
